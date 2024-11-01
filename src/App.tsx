cfdd218aa77d8be5e09b81e46e617ec8835fb0ac
Zack Piper <zpipe07@gmail.com>
2024-11-01 08:54:09 -0600

    interview setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,34 @@
-import { LogIn } from './LogIn'
-import { TicTacToe } from './TicTacToe'
-import { UsersList } from './UsersList'
-import { JokeGenerator } from './JokeGenerator'
-
 import './App.css'
 
 function App() {
   return (
-    <>
-      <h1>Hello, world!</h1>
-
-      <JokeGenerator />
-
-      <LogIn />
-
-      <TicTacToe />
-
-      <UsersList />
-    </>
+    <div
+      style={{
+        minHeight: '100vh',
+        maxWidth: '40rem',
+        margin: '0 auto',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <h1
+        style={{
+          border: '1px solid',
+          margin: 0,
+          padding: '1rem',
+          borderRadius: '0.5rem',
+        }}
+      >
+        Hello, world!
+      </h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum
+        laudantium asperiores vitae quos libero possimus modi quo nobis suscipit
+        eligendi.
+      </p>
+    </div>
   )
 }
 
